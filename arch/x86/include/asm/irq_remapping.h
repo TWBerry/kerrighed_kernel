6613a745c6e91d1fa4eb262ef33d8000131dc90c
TWBerry <thomaswilliamberry729@gmail.com>
2026-08-09 16:56:31 +0200

    x86/irq: make vcpu affinity fallback static inline

--- arch/x86/include/asm/irq_remapping.h
+++ arch/x86/include/asm/irq_remapping.h
@@ -62,7 +62,7 @@ extern bool setup_remapped_irq(int irq,
 			       struct irq_chip *chip);
 
 void irq_remap_modify_chip_defaults(struct irq_chip *chip);
-int irq_set_vcpu_affinity(unsigned int irq, void *vcpu_info);
+static inline int irq_set_vcpu_affinity(unsigned int irq, void *vcpu_info);
 
 enum {
 	IRQ_REMAP_XAPIC_MODE,
